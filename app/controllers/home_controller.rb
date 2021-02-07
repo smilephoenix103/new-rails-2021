7d063efa4bc7a102f7631d8237c68d7a49404bcb
smilephoenix103 <marugotomoaki@gmail.com>
2021-02-07 20:11:26 +0100

    Poprawki home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,21 +6,27 @@ class HomeController < ApplicationController
   	# @dataChart = "bar"
 
   	@chart = Chart.all
+  	select_char
 
-  	@chart.each do |c|
-  		puts c.inspect
-  	end
+  	# @chart.each do |c|
+  	# 	puts c.inspect
+  	# end
   	puts "!!!!!!!!!!TEST!!!!!!!!!!!!"
   	puts params[:chart_id]
   	puts "!!!!!!!!!!TEST!!!!!!!!!!!!"
 
-  	if (params[:chart_id] != nil)
-  		@dataChart = params[:chart_id]
-  	else
-  		@dataChart = "bar"
-  	end
+  	
   end
 
   def about
   end
+
+  private
+	  def select_char
+	  	if (params[:chart_id] != nil)
+  		@dataChart = params[:chart_id]
+  	else
+  		@dataChart = "bar"
+  	end
+	  end
 end
