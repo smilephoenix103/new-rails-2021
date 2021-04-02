ed4271a0538d0880a27a5fb0cf9aa1d14df640d9
smilephoenix103 <marugotomoaki@gmail.com>
2021-04-02 15:49:44 +0200

    Dodano selecty pzy dodawaniu banknotu: status, making, img_type. dodano walidacje pól

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,10 @@
 class Note < ApplicationRecord
   belongs_to :currency
 
+  validates :making, presence: true
+  validates :status, presence: true
+  validates_presence_of :img_type, presence: true, :message => 'Pole nie może byćpuste'
+
   def to_s
   	note
   end
---
+++ app/services/enums/select_enum.rb
@@ -0,0 +1,6 @@
+class SelectEnum < ApplicationRecord
+    # Enumerator
+    enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
+    enum making: ["Papier", "Polimer", "Hybryda", "Pozłacany", "Inny"]
+    enum img_type: ["SKAN", "FOTO", "LOC", "WWW"]
+  end
