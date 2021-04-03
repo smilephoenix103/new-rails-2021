d40f4bc7735b8f263009c7264f7d16eab84c3cc6
smilephoenix103 <marugotomoaki@gmail.com>
2021-04-03 14:35:38 +0200

    test class007

---
+++ app/services/enums/enum_add_item.rb
@@ -0,0 +1,18 @@
+class EnumAddItem
+    String @currency
+    String @code
+    Float @mid = 0
+    def initialize()
+    end
+    # def initialize(currency, code, mid)
+    #     @currency = currency
+    #     @code = code
+    #     @mid = mid
+    # end
+    attr_reader :currency, :code, :mid
+    attr_writer :currency, :code, :mid
+
+    def to_s
+        printf " %-35s %-10s %-10s",@currency, @code, @mid
+    end
+end
