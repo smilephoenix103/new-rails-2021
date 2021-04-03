cf1a3c0f3f8351280358137fe9f99b97fa4d84a8
smilephoenix103 <marugotomoaki@gmail.com>
2021-04-03 12:48:31 +0200

    poprawka enum

--- app/services/enums/select_enum.rb
+++ app/services/enums/select_enum.rb
@@ -3,4 +3,4 @@ class SelectEnum < ApplicationRecord
     enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
     enum making: ["Papier", "Polimer", "Hybryda", "Pozłacany", "Inny"]
     enum img_type: ["SKAN", "FOTO", "LOC", "WWW"]
-  end
+end
