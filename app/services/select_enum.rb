ef2f1fa7406a5e2e495d8dcd5c627586cc7ee4f0
smilephoenix103 <marugotomoaki@gmail.com>
2021-04-03 14:27:47 +0200

    test class005

--- app/services/select_enum.rb
+++ app/services/select_enum.rb
@@ -1,6 +1,3 @@
 class SelectEnum
-    # Enumerator
-    enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
-    enum making: ["Papier", "Polimer", "Hybryda", "Pozłacany", "Inny"]
-    enum img_type: ["SKAN", "FOTO", "LOC", "WWW"]
+ 
 end
