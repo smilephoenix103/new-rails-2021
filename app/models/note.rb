a9301f45f24a1c1d42e78b3c69a44f02a2494f7e
smilephoenix103 <marugotomoaki@gmail.com>
2021-04-05 16:00:02 +0200

    Test regex wprowadzania ceny zakupu

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,6 +5,9 @@ class Note < ApplicationRecord
   validates :status, presence: true
   validates :img_type, presence: true
 
+  # validates :price_buy, format: { with: /\A([0-9]{1,4}|[0-9]{1,4}+[\.]?[0-9]{1,2})\z/, message: "Cena musi byc liczba" }
+  validates :price_buy ,presence:true, numericality: {only_float: true}
+    
   def to_s
   	note
   end
