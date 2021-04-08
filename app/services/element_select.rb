385ae4b778cf07db830d200581b9a96a5b8dbe4c
smilephoenix103 <marugotomoaki@gmail.com>
2021-04-08 17:48:03 +0200

    poprawki Unum element_select

--- app/services/element_select.rb
+++ app/services/element_select.rb
@@ -2,8 +2,8 @@ class ElementSelect < ActiveRecord::Base
     # Enumerator
     enum status: ["KOLEKCJA", "FOR SELL", "SOLD", "RESERVATION"]
 
-    enum making: ["PAPIER", "POLIMER", "HYBRYDA"]
+    enum making: ["Papier", "Polimer", "Hybryda"]
 
-    enum img_type: ["SKAN", "FOTO", "LOC", "WWW" ]
+    enum img_type: ["Skan", "Foto", "LOC", "WWW" ]
 
 end
