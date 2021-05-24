3df43c979d182af6d805020b2e6e97b93aff1da5
smilephoenix103 <marugotomoaki@gmail.com>
2021-05-24 23:35:33 +0200

    Poprawki W note_sold

--- app/controllers/note_sold_controller.rb
+++ app/controllers/note_sold_controller.rb
@@ -9,5 +9,10 @@ class NoteSoldController < ApplicationController
 
     def show
         @notes = colection_notes_status(params[:id], "SOLD")
+        @profit = 0.0
+        @notes.each do |n|
+            @profit = @profit + (n.price_sell - n.price_buy)
+            puts @profit
+        end
     end
 end
