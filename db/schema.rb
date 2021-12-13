2e545d5252605b3b8469fc9196ae839b3743e911
smilephoenix103 <marugotomoaki@gmail.com>
2021-12-13 18:00:11 +0100

    poprawka rozmiaru

--- db/schema.rb
+++ db/schema.rb
@@ -171,10 +171,10 @@ ActiveRecord::Schema.define(version: 2021_20_92_220441) do
   end
 
   create_table "settings", force: :cascade do |t|
-    t.string "web", comment: "Do której strony dotyczą ustawienia"
-    t.string "name", comment: "Nazwa ustawień"
-    t.string "value", comment: "Wartość ustawień"
-    t.text "description", comment: "Opis ustawień"
+    t.string "web"
+    t.string "name"
+    t.string "value"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
