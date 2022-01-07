3eb2c3b923533a0bb0293d98e1bf55cc593615c1
smilephoenix103 <marugotomoaki@gmail.com>
2022-01-07 01:14:00 +0100

    testing new api to get current price metal ciąg dalszy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   include HomeHelper
+  require "#{Rails.root}/app/services/api_metals/metal.rb"
+  require "#{Rails.root}/app/services/api_metals/time_update.rb"
+
   def index
   	# @title = "test pecunia"
 
@@ -39,22 +42,48 @@ class HomeController < ApplicationController
   end
 
   def about
+    @time_update = Time_update.new
+    @metals = Array.new
     puts "******************************TEST*********************************"
     @response = RestClient.get('https://api.metals.live/v1/spot',
                                {'Content-Type' => 'application/json'}) {|response, request, result| response }
-    # puts @response.code
-    # puts @response.class.name
-    data = @response.body
-    result = JSON.parse(data)
-    puts JSON.parse(data)
-    @key = result[1].keys[0]
-    puts @key.to_s
-    puts @key.length
-    puts result[1][@key]
+    puts @response.code
+    puts @response.class.name
+    puts @response.net_http_res.inspect
 
-    result.each do |r|
-      puts r.keys[0].to_s + " - " + r[r.keys[0].to_s].to_s
-    end
+    @time_update.cod = @response.code
+    puts @time_update.inspect
+      data = @response.body
+      result = JSON.parse(data)
+      puts JSON.parse(data)
+      @key = result[1].keys[0]
+      # puts @key.to_s
+      # puts @key.length
+      # puts result[1][@key]
+
+
+      result.each do |r|
+        if r.keys[0].to_s == "timestamp"
+          puts" ---------------------------------------------"
+          # printf "| %-15s | %10s |\n" ,r.keys[0].to_s, DateTime.strptime(((r[r.keys[0]])/1000).to_s,'%s').strftime("%Y-%m-%d  %H:%M:%S")
+          printf "| %-15s | %25s |\n" ,r.keys[0].to_s, Time.at(((r[r.keys[0]])/1000))
+          @time_update.timestamp = r[r.keys[0]]
+          # puts Time.at(1641503597)
+          puts" ---------------------------------------------"
+        else
+          @metal = Metal.new
+          @metal.name = r.keys[0].to_s
+          @metal.price = r[(r.keys[0]).to_s]
+          @metals.push(@metal)
+          puts" ---------------------------------------------"
+          # printf "| %-15s | %10s |\n" , r.keys[0].to_s, r[(r.keys[0]).to_s].to_s
+          printf "| %-15s |%25s$ |\n" , r.keys[0].to_s, r[(r.keys[0]).to_s].to_s
+
+        end
+      end
+    puts @metals.inspect
+    @time_update.metals = @metals
+    puts @time_update.inspect
   end
 
   # private
