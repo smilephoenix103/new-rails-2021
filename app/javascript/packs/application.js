db9d1d2fa8187910da3a538688a81f60838e0668
smilephoenix103 <marugotomoaki@gmail.com>
2022-02-05 13:46:40 +0100

    poprawka test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import {BubbleController} from 'chart.js';
 
 
 Rails.start()
@@ -63,7 +62,7 @@ document.addEventListener('turbolinks:load', () => {
                 }
 
                 const ctx = document.getElementById('myChart').getContext('2d');
-                new Chart(ctx, {
+                const myChart = new Chart(ctx, {
                     // type: 'bar',
                     type: typeChart,
                     data: chartData,
